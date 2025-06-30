eb2061bd4d57f7a5330dee12a74a66185d873e5a
Abdullah Habib <abdullah2006habib@gmail.com>
2025-06-30 16:45:05 +0500

    change in lives logic

--- js/start.js
+++ js/start.js
@@ -4,13 +4,13 @@ import { AnimationRunner } from './frontEndRendering/animation.js';
 import { KeyTracker } from './bussinessLogic/keyTracker.js';
 import { DOMDisplay } from './frontEndRendering/domDisplay.js';
 import { Level } from './bussinessLogic/level.js';
-
+var lives = 3;
 class LevelManager {
 	static arrowKeyMap = { 37: 'left', 38: 'up', 39: 'right' };
 	constructor(levelPlans) {
 		this.levelPlans = levelPlans;
 		this.currentLevelIndex = 0;
-		this.lives = 3;
+		//this.lives = 3;
 		this._startLevel(this.currentLevelIndex);
 	}
 
@@ -35,9 +35,9 @@ class LevelManager {
 
 	_handleLevelEnd(status) {
 		if (status === 'lost') {
-			this.lives--;
-			
-			if(this.lives <= 0){
+			//this.lives--;
+			lives--;
+			if(lives <= 0){//this.lives <= 0){
 				window.location.href = "lost.html";
 			}
 
